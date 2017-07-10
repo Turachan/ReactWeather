3f7642a9e82401256c37b1014613d8248ac328d6
Appledore <turac.gurer@mebitech.com>
2017-07-10 11:47:26 +0300

    server.js configs

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(function(req, res, next){
-//   console.log(req.url);
-//   if (req.headers['X-Forwarded-Proto'] === 'https') {
-//     res.redirect('http://' + req.hostname +":"+PORT +  req.url);
-//   }
-//   else {
-//     next();
-//   }
-// });
-
-app.get(/\/register$/, function(req, res, next){
-  console.log(JSON.stringify(req.headers)); //to see all headers that heroku adds
-  if(req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "https") {
-    res.redirect("http://" + req.headers.host + req.url);
+app.use(function(req, res, next){
+  console.log(req.url);
+  if (req.headers['X-Forwarded-Proto'] === 'https') {
+    res.redirect('http://' + req.hostname +":"+PORT +  req.url);
   }
   else {
     next();
   }
 });
+
+// app.get(/\/register$/, function(req, res, next){
+//   console.log(JSON.stringify(req.headers)); //to see all headers that heroku adds
+//   if(req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "https") {
+//     res.redirect("http://" + req.headers.host + req.url);
+//   }
+//   else {
+//     next();
+//   }
+// });
 
 app.use(express.static('public'));
 
