7a4c07e4b372306bcc5ef5b4ba412689fb232fde
Appledore <turac.gurer@mebitech.com>
2017-07-10 11:33:19 +0300

    server.js configs

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(function(req, res, next){
-//   if (req.headers['x-forward-proto'] === 'http') {
-//     next();
-//   }
-//   else if (req.headers['x-forward-proto'] === 'https') {
-//     res.redirect('http://' + req.hostname +  req.url);
-//   }
-// });
+app.use(function(req, res, next){
+  console.log(req);
+  if (req.headers['x-forward-proto'] === 'https') {
+    res.redirect('http://' + req.hostname +":"+PORT +  req.url);
+  }
+  else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
